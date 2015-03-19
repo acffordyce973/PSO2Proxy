baf6c93481408a4ccd7666f34ffb4dd65c553beb
acffordyce973 <acffordyce973@users.noreply.github.com>
2015-03-19 15:37:15 +0000

    Update EQ_Notice.py

--- proxy/plugins/disabled/EQ_Notice.py
+++ proxy/plugins/disabled/EQ_Notice.py
@@ -309,8 +309,8 @@ class RequestEQNoitce(Command):
             argIn = self.args[8:].encode('utf-8')
             shipArg = int(argIn)
         except Exception as e:
-            print("[EQ Notice] Please enter the ship number to check.")
-            return
+            #print("[EQ Notice] Please enter the ship number to check.")
+            return("[EQ Notice] Please enter the ship number to check.")
         if 1 <= shipArg <= 10:
             if data_eq[shipArg - 1] and not check_if_EQ_old(shipArg - 1):
                 print("[EQ_Notice] Incoming EQ Report from PSO2es: %s" % (msg_eq[shipArg - 1]))
